b62e50d60381e1c490c0061882ab37e22d6367cf
KazeNoYumeX <foever527@gmail.com>
2024-07-14 20:58:24 +0800

    Style: Empty project for demo use

--- README.md
+++
@@ -1,2 +0,0 @@
-# 發病用
-[點擊查看網站](https://kazenoyumeX.github.io/999rrr)
--- dist/assets/AdminHome-c1b50081.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-81817b2a.js";const r={};function n(o,c){return"admin dashboard home"}const _=e(r,[["render",n]]);export{_ as default};
